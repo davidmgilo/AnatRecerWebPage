6768d1c7b103375635c1055fe6dd96113e1e931d
David Martínez Gilo <davidmgilo@gmail.com>
2017-04-28 10:44:46 +0200

    added 'spanish'

--- assets/js/carregar.js
+++ assets/js/carregar.js
@@ -3,12 +3,24 @@ $(function(){
     var loc = window.location.href;
     if(/cat/.test(loc)){
         $('#Nav').addClass('cat')
+        $('#RNav').addClass('cat')
         $('#home').attr('href','/assets/views/cat/principal.html')
         $('#group').attr('href','/assets/views/cat/about.html')
         $('#projects').attr('href','/assets/views/cat/projects.html')
         $('#publications').attr('href','/assets/views/cat/publish.html')
         $('#contacts').attr('href','/assets/views/cat/contact.html')
+        $('#media').attr('href','/assets/views/cat/media.html')
+    }else if (/es/.test(loc)){
+        $('#Nav').addClass('es')
+        $('#RNav').addClass('es')
+        $('#home').attr('href','/assets/views/es/principal.html')
+        $('#group').attr('href','/assets/views/es/about.html')
+        $('#projects').attr('href','/assets/views/es/projects.html')
+        $('#publications').attr('href','/assets/views/es/publish.html')
+        $('#contacts').attr('href','/assets/views/es/contact.html')
+        $('#media').attr('href','/assets/views/es/media.html')
     }else {
         $('#Nav').removeClass('cat')
+        $('#RNav').removeClass('cat')
     }
 })
--- assets/js/languages.js
+++ assets/js/languages.js
@@ -1,14 +1,29 @@
 function changeLan(lan){
     var nav = document.getElementById("Nav");
+    var rnav = document.getElementById("RNav");
     if(lan === 'English'){
         nav.classList.add('en');
+        rnav.classList.add('en');
         nav.classList.remove('cat');
+        rnav.classList.remove('cat');
         nav.classList.remove('es');
+        rnav.classList.remove('es');
         location.href='http://localhost:8080/'
     }else if (lan === 'Catala'){
         nav.classList.add('cat');
+        rnav.classList.add('cat');
         nav.classList.remove('en');
+        rnav.classList.remove('en');
         nav.classList.remove('es');
+        rnav.classList.remove('es');
         location.href='http://localhost:8080/assets/views/cat/principal.html' //TODO ref to server side
+    } else if (lan === 'Español'){
+        nav.classList.add('es');
+        rnav.classList.add('es');
+        nav.classList.remove('en');
+        rnav.classList.remove('en');
+        nav.classList.remove('cat');
+        rnav.classList.remove('cat');
+        location.href='http://localhost:8080/assets/views/es/principal.html' //TODO ref to server side
     }
 }
